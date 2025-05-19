e1e6e3762bd11d7ff2936abd8d7d364d9ca0b08d
Rahul <rahul1312ag@gmail.com>
2025-05-19 13:53:01 +0530

    WIP: Local edits to conv_sobel.v and hssim.v

--- arch2.0/hssim.v
+++ arch2.0/hssim.v
@@ -6,7 +6,7 @@ module HSSIM #(
 
     parameter NUMR_BIT_WIDTH = 36,
     parameter DENR_BIT_WIDTH = 36,
-    parameter NUMR_WIDTH = NUMR_BIT_WIDTH*PIXELS_PER_BEAT
+    parameter NUMR_WIDTH = NUMR_BIT_WIDTH*PIXELS_PER_BEAT,
     parameter DENR_WIDTH = DENR_BIT_WIDTH*PIXELS_PER_BEAT
 ){
     input clk,
@@ -33,8 +33,33 @@ mean calc -> 1 cycle dly => delay the result by 2 cycles
 variance(signed)  -> 3 cycle dly
 */
 
+localparam c1 = 6;
+localparam c2 = 58;
+
 localparam DLY_VAL_MEAN = 2;
 
+localparam DLY_VAL_MEAN = 2;
+
+
+reg [CONV_GAUSS_OUTPUT_WIDTH-1:0] out_mu_x_dly [0:DLY_VAL_MEAN-1];
+integer i;
+
+always @(posedge clk or negedge aresetn) begin
+    if (!aresetn) begin
+        for (i = 0; i < DLY_VAL_MEAN; i = i + 1) begin
+            out_mu_x_dly[i] <= 0;
+        end
+    end else if (!stall) begin
+        out_mu_x_dly[0] <= out_mu_x;
+        for (i = 1; i < DLY_VAL_MEAN; i = i + 1) begin
+            out_mu_x_dly[i] <= out_mu_x_dly[i-1];
+        end
+    end
+end
+
+// Delayed output
+wire [CONV_GAUSS_OUTPUT_WIDTH-1:0] out_mu_x_delayed;
+assign out_mu_x_delayed = out_mu_x_dly[DLY_VAL_MEAN-1];
 
 CONV_GAUSS #(PIXELS_PER_BEAT, CONV_GAUSS_INPUT_WIDTH, IMAGE_DIM) mu_x (clk, aresetn, stall, inp_edge, out_mu_x);
 CONV_GAUSS #(PIXELS_PER_BEAT, CONV_GAUSS_INPUT_WIDTH, IMAGE_DIM) mu_y (clk, aresetn, stall, ref_edge, out_mu_y);
