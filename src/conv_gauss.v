302293dbaf9fc8d09bd4d8390813723102752876
Sreenadh <sreenadh.vajrala@gmail.com>
2026-05-16 15:20:41 +0530

    update

--- src/conv_gauss.v
+++ src/conv_gauss.v
@@ -80,7 +80,6 @@ always @(posedge aclk) begin
             end
 
             m_axis_tvalid <= 1;
-            m_axis_tdata <= 3; // Placeholder for convolution result
 
             // 1 2 1
             // 2 4 2
@@ -88,18 +87,21 @@ always @(posedge aclk) begin
 
             // first output pixel of beat will be the conv output centered around last pixel of previous beat
             // for first 2 rows, there are no top rows for them and for first beat, there is no left pixel for first pixel in first beat of row
-            m_axis_tdata[0 +: PIXEL_SIZE] <= (row_ptr < 2 || col_ptr==0) ? 0 : ((last_top[0+: PIXEL_SIZE]   ) + (last_top[PIXEL_SIZE +: PIXEL_SIZE]<<1) + (buff_top_out[0+: PIXEL_SIZE]   ) + 
-                                              (last_mid[0+: PIXEL_SIZE]<<1) + (last_mid[PIXEL_SIZE +: PIXEL_SIZE]<<2) + (buff_mid_out[0+: PIXEL_SIZE]<<1) +
-                                              (last_bot[0+: PIXEL_SIZE]   ) + (last_bot[PIXEL_SIZE +: PIXEL_SIZE]<<1) + (s_axis_tdata[0+: PIXEL_SIZE]   )) >> 4;
+            m_axis_tdata[0 +: PIXEL_SIZE] <= (row_ptr < 2 || col_ptr==0) ? 0 : (
+                                              ({4'b0, last_top[0+: PIXEL_SIZE]}   ) + ({4'b0, last_top[PIXEL_SIZE +: PIXEL_SIZE]}<<1) + ({4'b0, buff_top_out[0+: PIXEL_SIZE]}   ) + 
+                                              ({4'b0, last_mid[0+: PIXEL_SIZE]}<<1) + ({4'b0, last_mid[PIXEL_SIZE +: PIXEL_SIZE]}<<2) + ({4'b0, buff_mid_out[0+: PIXEL_SIZE]}<<1) +
+                                              ({4'b0, last_bot[0+: PIXEL_SIZE]}   ) + ({4'b0, last_bot[PIXEL_SIZE +: PIXEL_SIZE]}<<1) + ({4'b0, s_axis_tdata[0+: PIXEL_SIZE]}   )) >> 4;
 
             // second output pixel of beat will be the conv output centered around first pixel of current beat
-            m_axis_tdata[PIXEL_SIZE +: PIXEL_SIZE] <= (row_ptr < 2 || col_ptr==0) ? 0 : ((last_top[PIXEL_SIZE +: PIXEL_SIZE]   ) + (buff_top_out[0 +: PIXEL_SIZE]<<1) + (buff_top_out[PIXEL_SIZE+: PIXEL_SIZE]   ) + 
-                                                       (last_mid[PIXEL_SIZE +: PIXEL_SIZE]<<1) + (buff_mid_out[0 +: PIXEL_SIZE]<<2) + (buff_mid_out[PIXEL_SIZE+: PIXEL_SIZE]<<1) +
-                                                       (last_bot[PIXEL_SIZE +: PIXEL_SIZE]   ) + (s_axis_tdata[0 +: PIXEL_SIZE]<<1) + (s_axis_tdata[PIXEL_SIZE+: PIXEL_SIZE]   )) >> 4;
+            m_axis_tdata[PIXEL_SIZE +: PIXEL_SIZE] <= (row_ptr < 2 || col_ptr==0) ? 0 : (
+                                                       ({4'b0, last_top[PIXEL_SIZE +: PIXEL_SIZE]}   ) + ({4'b0, buff_top_out[0 +: PIXEL_SIZE]}<<1) + ({4'b0, buff_top_out[PIXEL_SIZE+: PIXEL_SIZE]}   ) + 
+                                                       ({4'b0, last_mid[PIXEL_SIZE +: PIXEL_SIZE]}<<1) + ({4'b0, buff_mid_out[0 +: PIXEL_SIZE]}<<2) + ({4'b0, buff_mid_out[PIXEL_SIZE+: PIXEL_SIZE]}<<1) +
+                                                       ({4'b0, last_bot[PIXEL_SIZE +: PIXEL_SIZE]}   ) + ({4'b0, s_axis_tdata[0 +: PIXEL_SIZE]}<<1) + ({4'b0, s_axis_tdata[PIXEL_SIZE+: PIXEL_SIZE]}   )) >> 4;
             for(i = 2; i < PIXELS_PER_BEAT; i = i + 1) begin
-                m_axis_tdata[i*PIXEL_SIZE +: PIXEL_SIZE] <= (row_ptr < 2) ? 0 : ((buff_top_out[(i-2)*PIXEL_SIZE +: PIXEL_SIZE]) + (buff_top_out[(i-1)*PIXEL_SIZE +: PIXEL_SIZE]<<1) + (buff_top_out[i*PIXEL_SIZE +: PIXEL_SIZE]) + 
-                                                            (buff_mid_out[(i-2)*PIXEL_SIZE +: PIXEL_SIZE]<<1) + (buff_mid_out[(i-1)*PIXEL_SIZE +: PIXEL_SIZE]<<2) + (buff_mid_out[i*PIXEL_SIZE +: PIXEL_SIZE]<<1) +
-                                                            s_axis_tdata[(i-2)*PIXEL_SIZE +: PIXEL_SIZE] + (s_axis_tdata[(i-1)*PIXEL_SIZE +: PIXEL_SIZE]<<1) + s_axis_tdata[i*PIXEL_SIZE +: PIXEL_SIZE]) >> 4;
+                m_axis_tdata[i*PIXEL_SIZE +: PIXEL_SIZE] <= (row_ptr < 2) ? 0 : (
+                                                            ({4'b0, buff_top_out[(i-2)*PIXEL_SIZE +: PIXEL_SIZE]}     ) + ({4'b0, buff_top_out[(i-1)*PIXEL_SIZE +: PIXEL_SIZE]}<<1) + ({4'b0, buff_top_out[i*PIXEL_SIZE +: PIXEL_SIZE]}     ) + 
+                                                            ({4'b0, buff_mid_out[(i-2)*PIXEL_SIZE +: PIXEL_SIZE]}<<1) + ({4'b0, buff_mid_out[(i-1)*PIXEL_SIZE +: PIXEL_SIZE]}<<2) + ({4'b0, buff_mid_out[i*PIXEL_SIZE +: PIXEL_SIZE]}<<1) +
+                                                            ({4'b0, s_axis_tdata[(i-2)*PIXEL_SIZE +: PIXEL_SIZE]}     ) + ({4'b0, s_axis_tdata[(i-1)*PIXEL_SIZE +: PIXEL_SIZE]}<<1) + ({4'b0, s_axis_tdata[i*PIXEL_SIZE +: PIXEL_SIZE]}     )) >> 4;
             end
             last_top <= buff_top[col_ptr][DATA_WIDTH-1 -: 2*PIXEL_SIZE]; // last 2 pixels of current column beat of top buffer will be needed for convolution of next beat
             last_mid <= buff_mid[col_ptr][DATA_WIDTH-1 -: 2*PIXEL_SIZE];
