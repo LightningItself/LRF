07a43b4c95ef277699f9684d22f3efaa8d345807
Sreenadh <sreenadh_vajrala@gmail.com>
2026-06-13 00:23:41 +0530

    pushed buff of depth 1

--- src/axis_buff.v
+++ src/axis_buff.v
@@ -1,7 +1,6 @@
 module axis_buff #(
     parameter S_AXIS_DATA_WIDTH = 8,
-    parameter M_AXIS_DATA_WIDTH = 16,
-    parameter DEPTH = 1  // latency = DEPTH+1
+    parameter M_AXIS_DATA_WIDTH = 16
 )(
     input wire aclk,
     input wire aresetn,
@@ -10,48 +9,33 @@ module axis_buff #(
     input wire s_axis_tvalid,
     output wire s_axis_tready,
     input wire s_axis_tlast,
-
+    
     output reg [M_AXIS_DATA_WIDTH-1:0] m_axis_tdata,
     output reg m_axis_tvalid,
     input wire m_axis_tready,
     output reg m_axis_tlast
 );
 
-wire advance = ~m_axis_tvalid || m_axis_tready;
-
-reg [S_AXIS_DATA_WIDTH-1:0] data_pipe[DEPTH-1:0];
-reg valid_pipe[DEPTH-1:0];
-reg last_pipe[DEPTH-1:0];
-
-integer i;
 always @(posedge aclk) begin
     if(~aresetn) begin
         m_axis_tvalid <= 0;
-        m_axis_tlast  <= 0;
-        m_axis_tdata  <= 0;
-        for(i=0; i<DEPTH; i=i+1) begin
-            data_pipe[i]  <= 0;
-            valid_pipe[i] <= 0;
-            last_pipe[i]  <= 0;
-        end
+        m_axis_tlast <= 0;
+        m_axis_tdata <= 0;
     end
-    else if(advance) begin
-        if(s_axis_tvalid) begin
-            data_pipe[0] <= s_axis_tdata;
+    else begin
+        if(s_axis_tvalid && s_axis_tready) begin
+            m_axis_tvalid <= 1;
+            m_axis_tdata <= {M_AXIS_DATA_WIDTH{1'b0}} | s_axis_tdata;
+            m_axis_tlast <= s_axis_tlast;
         end
-        last_pipe[0] <= s_axis_tlast;
-        valid_pipe[0] <= s_axis_tvalid;
-        for(i=1; i<DEPTH; i=i+1) begin
-            data_pipe[i]  <= data_pipe[i-1];
-            valid_pipe[i] <= valid_pipe[i-1];
-            last_pipe[i]  <= last_pipe[i-1];
+        else if(m_axis_tvalid && m_axis_tready) begin
+            m_axis_tvalid <= 0;
+            m_axis_tlast <= 0;
+            m_axis_tdata <= 0;
         end
-        m_axis_tdata  <= {M_AXIS_DATA_WIDTH{1'b0}} | data_pipe[DEPTH-1];
-        m_axis_tvalid <= valid_pipe[DEPTH-1];
-        m_axis_tlast  <= last_pipe[DEPTH-1];
     end
 end
 
-assign s_axis_tready = advance;
+assign s_axis_tready = ~m_axis_tvalid || m_axis_tready;
 
 endmodule
