5f4e789cf436318613ea0f161d20d21cc1fe23e1
Sreenadh <sreenadh.vajrala@gmail.com>
2026-06-12 16:09:15 +0530

    removed the change

--- src/axis_buff.v
+++ src/axis_buff.v
@@ -1,7 +1,6 @@
 module axis_buff #(
     parameter S_AXIS_DATA_WIDTH = 8,
-    parameter M_AXIS_DATA_WIDTH = 16,
-    parameter DEPTH = 1 // latency = DEPTH+1
+    parameter M_AXIS_DATA_WIDTH = 16
 )(
     input wire aclk,
     input wire aresetn,
@@ -17,41 +16,26 @@ module axis_buff #(
     output reg m_axis_tlast
 );
 
-wire advance = ~m_axis_tvalid || m_axis_tready;
-
-reg [S_AXIS_DATA_WIDTH-1:0] data_buff[DEPTH-1:0];
-reg valid_buff[DEPTH-1:0];
-reg last_buff[DEPTH-1:0];
-
-integer i;
 always @(posedge aclk) begin
     if(~aresetn) begin
         m_axis_tvalid <= 0;
         m_axis_tlast <= 0;
         m_axis_tdata <= 0;
-        for(i=0; i<DEPTH; i=i+1) begin
-            data_buff[i] <= 0; 
-            valid_buff[i] <= 0; 
-            last_buff[i] <= 0;
-        end
     end
     else begin
-        if(advance) begin
-            data_buff[0] <= s_axis_tdata;
-            valid_buff[0] <= s_axis_tvalid;
-            last_buff[0] <= s_axis_tlast;
-            for(i=1; i<DEPTH; i=i+1) begin
-                data_buff[i] <= data_buff[i-1];
-                valid_buff[i] <= valid_buff[i-1];
-                last_buff[i] <= last_buff[i-1];
-            end
-        m_axis_tdata <= {M_AXIS_DATA_WIDTH{1'b0}} | data_buff[DEPTH-1];
-        m_axis_tvalid <= valid_buff[DEPTH-1];
-        m_axis_tlast <= last_buff[DEPTH-1];
+        if(s_axis_tvalid && s_axis_tready) begin
+            m_axis_tvalid <= 1;
+            m_axis_tdata <= {M_AXIS_DATA_WIDTH{1'b0}} | s_axis_tdata;
+            m_axis_tlast <= s_axis_tlast;
+        end
+        else if(m_axis_tvalid && m_axis_tready) begin
+            m_axis_tvalid <= 0;
+            m_axis_tlast <= 0;
+            m_axis_tdata <= 0;
         end
     end
-end 
+end
 
-assign s_axis_tready = advance;
+assign s_axis_tready = ~m_axis_tvalid || m_axis_tready;
 
 endmodule
