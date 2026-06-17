27f0eeaf77ddd58ca14816d4b88982e3d4be88b1
Sreenadh <sreenadh.vajrala@gmail.com>
2026-06-17 17:54:08 +0530

    updated

--- src/lsu.v
+++ src/lsu.v
@@ -24,51 +24,48 @@ localparam MEM_DEPTH = IMAGE_DIM * IMAGE_DIM / PIXELS_PER_BEAT;
 localparam ADDR_WIDTH = $clog2(MEM_DEPTH); 
 
 reg [DATA_WIDTH-1:0] ram [MEM_DEPTH-1:0];
-reg [ADDR_WIDTH:0] read_ptr, write_ptr;
 
-wire write_enable = s_axis_tvalid; 
+reg [ADDR_WIDTH:0] write_ptr, read_ptr;
 
-wire output_stage_advance = m_axis_tready || !m_axis_tvalid;
-wire data_available = (write_ptr > read_ptr);
-wire read_enable = data_available && output_stage_advance;
+wire empty = (write_ptr == read_ptr);
+wire full  = (write_ptr[ADDR_WIDTH] != read_ptr[ADDR_WIDTH]) && 
+             (write_ptr[ADDR_WIDTH-1:0] == read_ptr[ADDR_WIDTH-1:0]);
+
+assign s_axis_tready = !full;
+wire write_step = s_axis_tvalid & s_axis_tready;
+wire read_step  = m_axis_tready || !m_axis_tvalid;
 
 always @(posedge aclk) begin
     if(~aresetn) begin
         write_ptr <= 0; 
     end
-    else if(write_enable) begin
-        ram[write_ptr] <= s_axis_tdata;
+    else if(write_step) begin
+        ram[write_ptr[ADDR_WIDTH-1:0]] <= s_axis_tdata;
         write_ptr <= write_ptr + 1;
-    end
-    else if((write_ptr == MEM_DEPTH) & m_axis_tlast) begin
-        write_ptr <= 0;
     end
 end
 
 always @(posedge aclk) begin
     if(~aresetn) begin
-        read_ptr <= 0;
-        m_axis_tdata <= 0;
-        m_axis_tvalid <= 0;
-        m_axis_tlast <= 0;
+        read_ptr      <= 0;
+        m_axis_tdata  <= 0;
+        m_axis_tvalid <= 1'b0;
+        m_axis_tlast  <= 1'b0;
     end
-    else if (output_stage_advance) begin
-        if (read_enable) begin
-            m_axis_tdata <= ram[read_ptr];
-            m_axis_tlast <= (read_ptr == (MEM_DEPTH-1));
-            m_axis_tvalid <= (write_ptr > read_ptr);
-            if(read_ptr == (MEM_DEPTH-1)) begin
-                read_ptr <= 0;
-            end
-            else read_ptr <= read_ptr + 1;
+    else if (read_step) begin
+        if (!empty) begin
+            m_axis_tdata  <= ram[read_ptr[ADDR_WIDTH-1:0]];
+            m_axis_tvalid <= 1'b1;
+            
+            m_axis_tlast  <= (read_ptr[ADDR_WIDTH-1:0] == (MEM_DEPTH-1));
+            
+            read_ptr      <= read_ptr + 1;
         end
         else begin
             m_axis_tvalid <= 1'b0;
-            m_axis_tlast <= 1'b0;
+            m_axis_tlast  <= 1'b0;
         end
     end
 end
-
-assign s_axis_tready = 1'b1;
 
 endmodule
