ef52b8d72bcf921066e01da5fe78befba41d65df
Sreenadh <sreenadh.vajrala@gmail.com>
2026-06-20 19:17:45 +0530

    pushed work

--- src/top.v
+++ src/top.v
@@ -26,145 +26,166 @@ localparam BEATS_PER_IMAGE = IMAGE_DIM*IMAGE_DIM/PIXELS_PER_BEAT;
 localparam BEAT_COUNTER_BITS = $clog2(BEATS_PER_IMAGE);
 localparam FRAME_COUNTER_BITS = $clog2(2*FUSE_COUNT-1);
 
-reg [FRAME_COUNTER_BITS-1:0] frame_counter;
+// Frames are coming alternatively as new -> old -> new -> old ......
+// States change based on tlast
 
-wire advance = (m_axis_tready || ~m_axis_tvalid);
+// State switching logic 
 
-// Counter logic (BEAT COUNTER not req as it was used to sync the latencies of modules and counters)
-always @(posedge s_axis_aclk) begin
-    if(~s_axis_aresetn) begin
-        frame_counter <= 0;
-    end
-    else if(s_axis_tready & s_axis_tvalid & s_axis_tlast) begin
-        frame_counter <= frame_counter+1;
-    end
-end
+localparam IDLE = 2'b00;
+localparam NEW  = 2'b01;
+localparam OLD  = 2'b10;
 
-// Average buffer logic
-reg avg_first, avg_add;
-
-reg avg_first_buff, avg_add_buff;
-
-reg [DATA_WIDTH+(N_FUSE_COUNT+1)*PIXELS_PER_BEAT-1:0] avg_buff_in, avg_frame_buff_out;
-reg [PIXELS_PER_BEAT-1:0] avg_buff_in_valid, avg_buff_in_last;
-wire avg_buff_in_valid_wire = &avg_buff_in_valid;
-wire avg_buff_in_last_wire = &avg_buff_in_last;
-
-reg [DATA_WIDTH-1:0] avg_frame;
-reg [PIXELS_PER_BEAT-1:0] avg_frame_valid, avg_frame_last;
-reg [DATA_WIDTH+(N_FUSE_COUNT+1)*PIXELS_PER_BEAT-1:0] iframex17, iframe;
-reg [PIXELS_PER_BEAT-1:0] iframex17_valid, iframex17_last, iframe_valid, iframe_last;
-wire avg_frame_valid_wire = &avg_frame_valid;
-wire avg_frame_last_wire = &avg_frame_last;
-wire iframex17_valid_wire = &iframex17_valid;
-wire iframex17_last_wire = &iframex17_last;
-wire iframe_valid_wire = &iframe_valid;
-wire iframe_last_wire = &iframe_last;
-
-wire ready = (advance || !avg_buff_in_valid_wire);
-
-reg avg_out_state, avg_next_state;
-reg avg_curr_en, avg_next_en;
-reg avg_en_a, avg_en_b;
-
-always @(posedge s_axis_aclk) begin
-    if(~s_axis_aresetn) begin
-        avg_first <= 1;
-        avg_add <= 0;
-    end
-    else if (s_axis_tvalid & s_axis_tready & s_axis_tlast) begin
-        avg_add <= ~avg_add;
-        avg_first <= 0;
-    end
-end
+reg [1:0] state;
 
 always @(posedge s_axis_aclk) begin
     if(!s_axis_aresetn) begin
-        avg_first_buff <= 0;
-        avg_add_buff <= 0;
+        state <= IDLE;
     end
-    else if(s_axis_tvalid & s_axis_tready) begin
-        avg_first_buff <= avg_first;
-        avg_add_buff <= avg_add;
+    else begin
+        case(state)
+            IDLE: begin
+                if(s_axis_tvalid & s_axis_tready) begin
+                    state <= NEW;
+                end
+            end
+            NEW: begin
+                if(s_axis_tlast & s_axis_tvalid & s_axis_tready) begin
+                    state <= OLD;
+                end
+            end
+            OLD: begin
+                if(sub_last & sub_valid & avg_lsu_ready) begin
+                    state <= NEW;
+                end
+            end
+            default: begin
+                state <= IDLE;
+            end
+        endcase
+    end
+end
+
+// LOGIC FOR OLD STATE,  where the oldest frame comes into system,  here we only try to update the average and Fusion have no role in this state
+// In the OLD state, fetch -> sub -> send it back (as a loop). So we need to axis_sub, LSU (common for 2 states). 
+// Fetch have 1, sub have 1 and send it back have 1 cycle latency. LSU initialization wont be in this state. 
+
+wire [DATA_WIDTH+(N_FUSE_COUNT+1)*PIXELS_PER_BEAT-1:0] avg_buff_in, avg_buff_out;
+wire avg_buff_in_valid, avg_buff_in_last, avg_buff_out_valid, avg_buff_out_last;
+wire avg_lsu_ready;
+
+reg [DATA_WIDTH-1:0] data_buff;
+reg valid_buff, last_buff;
+
+wire [DATA_WIDTH+(N_FUSE_COUNT+1)*PIXELS_PER_BEAT-1:0] int_sub;
+wire [PIXELS_PER_BEAT-1:0] int_sub_valid, int_sub_last, sub_ready_a, sub_ready_b;
+wire sub_ready = (&sub_ready_a) & (&sub_ready_b);
+wire sub_valid = &int_sub_valid;
+wire sub_last = &int_sub_last;
+assign avg_buff_in = int_sub;
+assign avg_buff_in_valid = sub_valid;
+assign avg_buff_in_last = sub_last;
+
+reg gate;
+
+LSU #( .PIXELS_PER_BEAT(PIXELS_PER_BEAT), .IMAGE_DIM(IMAGE_DIM), .BIT_WIDTH(9+N_FUSE_COUNT)) average(
+    .aclk(s_axis_aclk),
+    .aresetn(s_axis_aresetn),
+    .s_axis_tdata(avg_buff_in),
+    .s_axis_tvalid(avg_buff_in_valid),
+    .s_axis_tready(avg_lsu_ready),
+    .s_axis_tlast(avg_buff_in_last),
+    .m_axis_tdata(avg_buff_out),
+    .m_axis_tvalid(avg_buff_out_valid),
+    .m_axis_tready(sub_ready & ~old_input_done),
+    .m_axis_tlast(avg_buff_out_last) 
+);
+
+// avg_buff_out will have one cycle latency, so either we need to prefetch the data or delay the beat into sub by one cycle
+// assuming when the state changes to OLD, the read pointer of LSU moved to 0 on same cycle
+// Lets delay the beat by one cycle
+
+always @(posedge s_axis_aclk) begin
+    if(!s_axis_aresetn) begin
+        data_buff <= 0;
+        valid_buff <= 0;
+        last_buff <= 0;
+    end
+    else begin
+        if(sub_ready || (!avg_buff_out_valid & gate)) begin
+            data_buff <= s_axis_tdata;
+            valid_buff <= s_axis_tvalid;
+            last_buff <= s_axis_tlast;
+        end
     end
 end
 
 genvar i;
 generate 
     for(i=0; i<PIXELS_PER_BEAT; i=i+1) begin
-        always @(posedge s_axis_aclk) begin
-            if(~s_axis_aresetn) begin
-                iframex17[(9+N_FUSE_COUNT)*i+:(9+N_FUSE_COUNT)] <= 0;
-                iframex17_valid[i] <= 0;
-                iframex17_last[i] <= 0;
-                iframe[(9+N_FUSE_COUNT)*i+:8] <= 0; 
-                iframe[((9+N_FUSE_COUNT)*i+8)+:(N_FUSE_COUNT+1)] <= 0;
-                iframe_valid[i] <= 0;
-                iframe_last[i] <= 0;
-                avg_frame[(8*i)+:8] <= 0;
-                avg_frame_valid[i] <= 0;
-                avg_frame_last[i] <= 0;
-            end
-            else begin
-                if(s_axis_tvalid & s_axis_tready) begin
-                    iframex17[(9+N_FUSE_COUNT)*i+:(9+N_FUSE_COUNT)] <= (s_axis_tdata[8*i+:8]<<N_FUSE_COUNT) + s_axis_tdata[8*i+:8];
-                    iframex17_valid[i] <= 1;
-                    iframex17_last[i] <= s_axis_tlast;
-                    iframe[(9+N_FUSE_COUNT)*i+:8] <= s_axis_tdata[8*i+:8]; 
-                    iframe[((9+N_FUSE_COUNT)*i+8)+:(N_FUSE_COUNT+1)] <= 0; 
-                    iframe_valid[i] <= 1;
-                    iframe_last[i] <= s_axis_tlast;
-                    avg_frame[(8*i)+:8] <= (avg_first) ? s_axis_tdata[(8*i)+:8] : avg_frame_buff_out[((9+N_FUSE_COUNT)*i+N_FUSE_COUNT)+:8];
-                    avg_frame_valid[i] <= 1;
-                    avg_frame_last[i] <= s_axis_tlast;
-                end
-                else if(ready & iframex17_valid_wire & iframe_valid_wire) begin
-                    iframex17_valid[i] <= 0;
-                    iframex17_last[i] <= 0;
-                    iframe_valid[i] <= 0;
-                    iframe_last[i] <= 0;
-                    avg_frame_valid[i] <= 0;
-                    avg_frame_last[i] <= 0;
-                end
-            end
-        end
-    end    
+        AXIS_UNSIGN_SUB #( .DATA_WIDTH(9+N_FUSE_COUNT)) sub(
+            .aclk(s_axis_aclk),
+            .aresetn(s_axis_aresetn),
+            .s_axis_a_tdata(avg_buff_out[i*(9+N_FUSE_COUNT)+:(9+N_FUSE_COUNT)]),
+            .s_axis_a_tvalid(avg_buff_out_valid & gate),
+            .s_axis_a_tready(sub_ready_a[i]),
+            .s_axis_a_tlast(avg_buff_out_last),
+            .s_axis_b_tdata({ {(N_FUSE_COUNT+1){1'b0}}, data_buff[i*PIXEL_SIZE+:PIXEL_SIZE] }),
+            .s_axis_b_tvalid(valid_buff & gate),
+            .s_axis_b_tready(sub_ready_b[i]),
+            .s_axis_b_tlast(last_buff),
+            .m_axis_tdata(int_sub[i*(9+N_FUSE_COUNT)+:(9+N_FUSE_COUNT)]),
+            .m_axis_tvalid(int_sub_valid[i]),
+            .m_axis_tready(avg_lsu_ready),
+            .m_axis_tlast(int_sub_last[i])
+        );
+    end
 endgenerate
 
-// converging point
+// lets say the system got last beat of the oldest frame, it will be read after 1 cycle, sub on 2nd cycle, written back on 3rd cycle
+// do i need wait to in this state, for 3 cycles? or safely move to NEW state, as NEW state will be just reading the LSU but not writing anything
+// but for this to happen, i should keep the sub active until the last updated beat is obtained from sub. 
+// so i will be in OLD state until my sub_last came out and also LSU is ready to take it, so that on the next cycle when my LSU gets updated and at the 
+// same time, my state is moved to NEW
 
-integer j;
+// old state perspective : start -> gate should be 1, stop -> gate should be 0 
+// sub master ready shouldnt be gated by gate, because if depends, once gate is 0, it wont advance so the data will be stuck
+// Thinking only in old state and not bothering about NEW state now
+// gate should go low in next egde if in this edge i got last_buff and sub is ready and accepted it and i should be in old state
+// gate should be high in edge edge if in this edge i got s_axis_tlast and i should be in new state (something other logic will be there)
+
 always @(posedge s_axis_aclk) begin
     if(!s_axis_aresetn) begin
-        avg_buff_in <= 0;
-        avg_buff_in_valid <= 0;
-        avg_buff_in_last <= 0;
+        gate <= 0;
     end
     else begin
-        if(ready & iframex17_valid_wire & iframe_valid_wire) begin
-            for(j=0; j<PIXELS_PER_BEAT; j=j+1) begin
-                if(avg_first_buff) begin
-                    avg_buff_in[(9+N_FUSE_COUNT)*j+:(9+N_FUSE_COUNT)] <= iframex17[(9+N_FUSE_COUNT)*j+:(9+N_FUSE_COUNT)];
-                    avg_buff_in_valid[j] <= 1'b1;
-                    avg_buff_in_last[j] <= iframex17_last[j]; 
-                end
-                else if(avg_add_buff) begin
-                    avg_buff_in[(9+N_FUSE_COUNT)*j+:(9+N_FUSE_COUNT)] <= avg_frame_buff_out[(9+N_FUSE_COUNT)*j+:(9+N_FUSE_COUNT)] - iframe[(9+N_FUSE_COUNT)*j+:(9+N_FUSE_COUNT)];
-                    avg_buff_in_valid[j] <= 1'b1;
-                    avg_buff_in_last[j] <= iframe_last[j]; 
-                end
-                else begin
-                    avg_buff_in[(9+N_FUSE_COUNT)*j+:(9+N_FUSE_COUNT)] <= avg_frame_buff_out[(9+N_FUSE_COUNT)*j+:(9+N_FUSE_COUNT)] + iframe[(9+N_FUSE_COUNT)*j+:(9+N_FUSE_COUNT)];
-                    avg_buff_in_valid[j] <= 1'b1;
-                    avg_buff_in_last[j] <= iframe_last[j]; 
-                end
-            end
+        if((state == OLD) & last_buff & sub_ready) begin
+            gate <= 0;
         end
-        else if(some_ready_from_lsu & avg_buff_in_valid_wire) begin
-            avg_buff_in_valid <= 0;
-            avg_buff_in_last <= 0; 
+        else if (state == OLD) begin
+            gate <= 1;
         end
     end
 end
+
+// once the tlast of input stream comes, i will fetch the last beat from LSU on next cycle and then stop fetching from LSU in old state
+// since i made the 
+// and once if i am OLD state and got tlast, i should not be ready until i change my state to NEW
+
+reg old_input_done;
+
+always @(posedge s_axis_aclk) begin
+    if (!s_axis_aresetn) begin
+        old_input_done <= 1'b0;
+    end else begin
+        if ((state == OLD) && s_axis_tvalid && s_axis_tready && s_axis_tlast) begin
+            old_input_done <= 1'b1;
+        end 
+        else if (state == NEW) begin
+            old_input_done <= 1'b0;
+        end
+    end
+end
+
+assign s_axis_tready = (state == IDLE) ? 1'b1 : (state == NEW)  ? m_axis_tready : (state == OLD)  ? (sub_ready && !old_input_done) : 1'b0;
 
 endmodule
