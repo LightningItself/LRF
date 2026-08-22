867307f7f54b10188c1d7988464926adbe88167f
Sreenadh <sreenadh.vajrala@gmail.com>
2026-08-22 19:39:08 +0530

    verified commit

--- src/sobel_hssim_top.v
+++ src/sobel_hssim_top.v
@@ -26,21 +26,20 @@ module SOBEL_HSSIM_TOP #(
     output reg              del_last
 );
 
-// Wire Declarations
-
-wire advance = (del_ready || ~del_valid);
+wire advance = (del_ready | ~del_valid);
 wire inputs_valid = old_map_valid & avg_map_valid & new_map_valid;
 
 wire [DATA_WIDTH-1:0] old_edge, avg_edge, new_edge;
 wire old_edge_valid, old_edge_last, avg_edge_valid, avg_edge_last, new_edge_valid, new_edge_last;
 wire sob_old_ready, sob_avg_ready, sob_new_ready;
-wire sobs_ready = sob_old_ready & sob_avg_ready & sob_new_ready;
+
+wire [DATA_WIDTH-1:0] old_edge_skid, avg_edge_skid, new_edge_skid;
+wire old_edge_skid_valid, old_edge_skid_last, avg_edge_skid_valid, avg_edge_skid_last, new_edge_skid_valid, new_edge_skid_last;
+wire old_edge_skid_ready, avg_edge_skid_ready, new_edge_skid_ready;
 
 wire [DATA_WIDTH-1:0] del_int;
 wire del_int_valid, del_int_last;
 wire hssim_ready_x, hssim_ready_y, hssim_ready_z;
-
-// DataPath
 
 CONV_SOBEL #( .PIXELS_PER_BEAT(PIXELS_PER_BEAT), .PIXEL_SIZE(PIXEL_SIZE), .IMAGE_WIDTH(IMAGE_DIM), .DATA_WIDTH(DATA_WIDTH)) sob_old(
     .aclk(aclk),
@@ -51,7 +50,7 @@ CONV_SOBEL #( .PIXELS_PER_BEAT(PIXELS_PER_BEAT), .PIXEL_SIZE(PIXEL_SIZE), .IMAGE
     .s_axis_tlast(old_map_last),
     .m_axis_tdata(old_edge),
     .m_axis_tvalid(old_edge_valid),
-    .m_axis_tready(hssim_ready_x),
+    .m_axis_tready(old_edge_skid_ready),
     .m_axis_tlast(old_edge_last)
 );
 
@@ -64,7 +63,7 @@ CONV_SOBEL #( .PIXELS_PER_BEAT(PIXELS_PER_BEAT), .PIXEL_SIZE(PIXEL_SIZE), .IMAGE
     .s_axis_tlast(avg_map_last),
     .m_axis_tdata(avg_edge),
     .m_axis_tvalid(avg_edge_valid),
-    .m_axis_tready(hssim_ready_y),
+    .m_axis_tready(avg_edge_skid_ready),
     .m_axis_tlast(avg_edge_last)
 );
 
@@ -77,25 +76,64 @@ CONV_SOBEL #( .PIXELS_PER_BEAT(PIXELS_PER_BEAT), .PIXEL_SIZE(PIXEL_SIZE), .IMAGE
     .s_axis_tlast(new_map_last),
     .m_axis_tdata(new_edge),
     .m_axis_tvalid(new_edge_valid),
-    .m_axis_tready(hssim_ready_z),
+    .m_axis_tready(new_edge_skid_ready),
     .m_axis_tlast(new_edge_last)
+);
+
+axis_skid_buff #( .DATA_WIDTH(DATA_WIDTH)) old_edge_skid_buff(
+    .aclk(aclk),
+    .aresetn(aresetn),
+    .s_axis_tdata(old_edge),
+    .s_axis_tvalid(old_edge_valid),
+    .s_axis_tready(old_edge_skid_ready),
+    .s_axis_tlast(old_edge_last),
+    .m_axis_tdata(old_edge_skid),
+    .m_axis_tvalid(old_edge_skid_valid),
+    .m_axis_tready(hssim_ready_x),
+    .m_axis_tlast(old_edge_skid_last)
+);
+
+axis_skid_buff #( .DATA_WIDTH(DATA_WIDTH)) avg_edge_skid_buff(
+    .aclk(aclk),
+    .aresetn(aresetn),
+    .s_axis_tdata(avg_edge),
+    .s_axis_tvalid(avg_edge_valid),
+    .s_axis_tready(avg_edge_skid_ready),
+    .s_axis_tlast(avg_edge_last),
+    .m_axis_tdata(avg_edge_skid),
+    .m_axis_tvalid(avg_edge_skid_valid),
+    .m_axis_tready(hssim_ready_y),
+    .m_axis_tlast(avg_edge_skid_last)
+);
+
+axis_skid_buff #( .DATA_WIDTH(DATA_WIDTH)) new_edge_skid_buff(
+    .aclk(aclk),
+    .aresetn(aresetn),
+    .s_axis_tdata(new_edge),
+    .s_axis_tvalid(new_edge_valid),
+    .s_axis_tready(new_edge_skid_ready),
+    .s_axis_tlast(new_edge_last),
+    .m_axis_tdata(new_edge_skid),
+    .m_axis_tvalid(new_edge_skid_valid),
+    .m_axis_tready(hssim_ready_z),
+    .m_axis_tlast(new_edge_skid_last)
 );
 
 HSSIM_TOP #( .PIXELS_PER_BEAT(PIXELS_PER_BEAT), .PIXEL_SIZE(PIXEL_SIZE), .IMAGE_DIM(IMAGE_DIM)) hssim(
     .aclk(aclk),
     .aresetn(aresetn),
-    .old_map(old_edge),
-    .old_map_valid(old_edge_valid),
+    .old_map(old_edge_skid),
+    .old_map_valid(old_edge_skid_valid),
     .old_map_ready(hssim_ready_x),
-    .old_map_last(old_edge_last),
-    .avg_map(avg_edge),
-    .avg_map_valid(avg_edge_valid),
+    .old_map_last(old_edge_skid_last),
+    .avg_map(avg_edge_skid),
+    .avg_map_valid(avg_edge_skid_valid),
     .avg_map_ready(hssim_ready_y),
-    .avg_map_last(avg_edge_last),
-    .new_map(new_edge),
-    .new_map_valid(new_edge_valid),
+    .avg_map_last(avg_edge_skid_last),
+    .new_map(new_edge_skid),
+    .new_map_valid(new_edge_skid_valid),
     .new_map_ready(hssim_ready_z),
-    .new_map_last(new_edge_last),
+    .new_map_last(new_edge_skid_last),
     .del(del_int),
     .del_valid(del_int_valid),
     .del_ready(advance),
@@ -121,8 +159,8 @@ always@(posedge aclk) begin
     end
 end
 
-assign old_map_ready = sobs_ready & avg_map_valid & new_map_valid;
-assign avg_map_ready = sobs_ready & old_map_valid & new_map_valid;
-assign new_map_ready = sobs_ready & old_map_valid & avg_map_valid;
+assign old_map_ready = sob_old_ready & avg_map_valid & new_map_valid;
+assign avg_map_ready = sob_avg_ready & old_map_valid & new_map_valid;
+assign new_map_ready = sob_new_ready & old_map_valid & avg_map_valid;
 
 endmodule
